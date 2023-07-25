7cfc9bc746ab4415dda5cc2052f0420dab2039e0
BGP\luiz.sales <lsales@noovi.com.br>
2023-07-25 14:44:46 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const exampleTable = document.querySelector('#example-table');
+exampleTable.style.overflowX = 'auto';
+exampleTable.style.width = '80%';
+exampleTable.style.height = '90%';
+    
+const bodyDOM = document.querySelector('body');
+bodyDOM.style.width = '100vw'
+bodyDOM.style.height = '100vh'
