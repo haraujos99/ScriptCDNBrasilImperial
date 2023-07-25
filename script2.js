2e093441d9e46718b521ee6b1fbf181f2c7ef6d6
BGP\luiz.sales <lsales@noovi.com.br>
2023-07-25 15:13:57 -0300

    script2

---
+++ script2.js
@@ -0,0 +1,9 @@
+const bodyDOM = document.querySelector('body');
+bodyDOM.style.width = '60vw'
+bodyDOM.style.height = '100vh'
+
+const exampleTable = document.querySelector('#example-table');
+exampleTable.style.width = '100%';
+exampleTable.style.height = '100%';
+//exampleTable.style.overflowX = 'auto';
+//exampleTable.style.whiteSpace = 'nowrap'
