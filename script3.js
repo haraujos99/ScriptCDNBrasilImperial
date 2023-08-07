0561ac37fbcd858b13ee1fed537f22abf0f49608
BGP\luiz.sales <lsales@noovi.com.br>
2023-08-07 09:33:47 -0300

    Alteração final

---
+++ script3.js
@@ -0,0 +1,15 @@
+const bodyDOM = document.querySelector('body');
+bodyDOM.style.margin= '0px';
+
+const centralizeDiv = document.querySelector('.centralize');
+centralizeDiv.style.width = '100vw';
+centralizeDiv.style.height = '90vh';
+centralizeDiv.style.display = 'flex';
+centralizeDiv.style.alignItems = 'center';
+centralizeDiv.style.justifyContent = 'center';
+
+const exampleTable = document.querySelector('#example-table');
+exampleTable.style.width = '60%';
+exampleTable.style.height = '80%';
+exampleTable.style.overflowX = 'auto';
+exampleTable.style.whiteSpace = 'nowrap';
